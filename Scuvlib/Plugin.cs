f06722b766f48f636d8452a8ae245629aa81db22
MSI <MSI@DESKTOP-NMO8DRH>
2025-04-23 17:42:42 +0300

    Добавьте файлы проекта.

---
+++ Scuvlib/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.Rendering;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace SkateScav
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string modGUID = "05126619z.SkateScav";
+        private const string modName = "SkateScav";
+        private const string modVersion = "1.0.0";
+
+        public static ManualLogSource logger;
+        private Harmony harmony;
+        public static AssetBundle MyAssetBundle { get; private set; }
+        public static string AssetsFolderPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Assets");
+
+        public void Awake()
+        {
+            MyAssetBundle = AssetBundle.LoadFromFile(Path.Combine(AssetsFolderPath, "myassetbundle"));
+            logger = Logger;
+            //logger.LogInfo(Plugin.MyAssetBundle.LoadAsset<AudioClip>("assets/sounds/lobotomy.mp3"));
+            harmony = new Harmony(modGUID);
+            harmony.PatchAll();
+            Logger.LogInfo($"Plugin {modName} is loaded!");
+        }
+
+    }
+}
